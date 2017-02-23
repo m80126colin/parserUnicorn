c940b698257d6db8984db7b02b84add9f385cb3a
Karinsu <m80126colin@gmail.com>
2017-02-23 20:57:41 +0800

    modified Intersect

--- src/components/Intersect.vue
+++ src/components/Intersect.vue
@@ -20,7 +20,7 @@
     </header>
     <h3 class="ui header">統計</h3>
     <div class="ui list">
-      <div class="item" v-for="(tot, i) in result.article_count">檔案 {{ i }}: {{ tot }} 筆</div>
+      <div class="item" v-for="(tot, i) in result.count">檔案 {{ i }}: {{ tot }} 筆</div>
     </div>
     <h3 class="ui header">共同交集</h3>
     <table class="ui definition table">
@@ -33,7 +33,7 @@
       <tbody>
         <tr v-for="(row, i) in result.article">
           <td>檔案 {{ i }}</td>
-          <td v-for="(d, j) in row" v-if="j < i">{{ d.length }}</td>
+          <td v-for="(d, j) in row" v-if="j < i">{{ d }}</td>
           <td v-else></td>
         </tr>
       </tbody>
@@ -43,31 +43,22 @@
     <header class="ui dividing header">
       <h2>ID 交集</h2>
     </header>
+    <div class="ui form">
+      <div class="inline field">
+        <label for="limit">篩選數量</label>
+        <input type="number" min="1" v-model="limit" />
+      </div>
+    </div>
     <div v-if="result.link">
       <a class="ui primary button"
         target="_blank" 
         v-if="result.link"
-        :href="result.link">下載</a>
+        :href="result.link">下載矩陣</a>
+      <a class="ui primary button"
+        target="_blank" 
+        v-if="result.link2"
+        :href="result.link2">下載資料</a>
     </div>
-    <div class="ui form">
-      <div class="inline field">
-        <label for="gap">計數條件</label>
-        <input type="number" name="gap" min="0" :max="gapmax" v-model="gap" />
-        <label for="gap">最大值: {{ gapmax }}</label>
-      </div>
-    </div>
-    <table class="ui definition table" v-if="result.people">
-      <thead>
-        <th></th>
-        <th v-for="(h, i) in tableheader" :data-tooltip="result.list[ h[0] ][0]">{{ i }}</th>
-      </thead>
-      <tbody>
-        <tr v-for="(row, i) in tablebody">
-          <td :data-tooltip="result.list[ tableheader[i][0] ][0]">{{ i }}</td>
-          <td v-for="d in row">{{ d }}</td>
-        </tr>
-      </tbody>
-    </table>
   </article>
 </section>
 </template>
@@ -85,51 +76,13 @@ export default {
       files: [
         { id: 0 }
       ],
-      link:  '',
-      link2: '',
       datarows: [],
       result: {},
-      gap: 0
+      limit: 100
     }
   },
   created() {
     this.addRow()
-  },
-  computed: {
-    people() {
-      const app = this
-      return _.map(
-        app.result.peo_list,
-        (people, idx) => [people, idx])
-    },
-    gapmax() {
-      const app = this
-      if (app.result.list)
-        return _.maxBy(app.result.list, people => people[1])[1]
-      return 0
-    },
-    idlist() {
-      const app = this
-      return _.filter(
-        app.people,
-        obj => app.result.list[ obj[0] ][1] >= app.gap)
-    },
-    tableheader() {
-      const app = this
-      return _.take(app.idlist, 15)
-    },
-    tablebody() {
-      const app = this
-      let res = []
-      for (let i = 0; i < app.tableheader.length; i++) {
-        let tmp = []
-        for (let j = 0; j < app.tableheader.length; j++) {
-          tmp.push(app.result.people[ app.tableheader[i][1] ][ app.tableheader[j][1] ])
-        }
-        res.push(tmp)
-      }
-      return res
-    }
   },
   methods: {
     addRow(e) {
@@ -140,8 +93,9 @@ export default {
       this.datarows.splice(idx, 1)
     },
     intersectHandler(e) {
-      let app = this
+      const app = this
       let formData = new FormData( $('#form')[0] )
+      formData.set('limit', app.limit)
 
       $.ajax({
         type:        'POST',
